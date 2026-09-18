80aa8395743be2b7472ee0e3094bc6a3bf4920cd
Rodrigo Apcho <135272939+TheAlejo160@users.noreply.github.com>
2026-09-18 18:54:16 -0500

    Initial Commit

---
+++ Adapta-PE/Adapta_PEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adapta_PEApp.swift
+//  Adapta-PE
+//
+//  Created by Rodrigo Alejandro Apcho Aliaga on 18/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Adapta_PEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adapta-PETests/Adapta_PETests.swift
@@ -0,0 +1,19 @@
+//
+//  Adapta_PETests.swift
+//  Adapta-PETests
+//
+//  Created by Rodrigo Alejandro Apcho Aliaga on 18/09/26.
+//
+
+import Testing
+@testable import Adapta_PE
+
+struct Adapta_PETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
